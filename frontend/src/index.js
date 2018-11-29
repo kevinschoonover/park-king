79e499c807a850e0a158a969e05d9981736d0a74
Kevin Schoonover <schoonoverkevinm@gmail.com>
2018-11-29 16:26:52 -0600

    Add sign in page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
 import Dashboard from './dashboard/Dashboard';
+import SignIn from './sign-in/SignIn'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
     <div style={{height: '100%'}}>
       <Route exact path="/" component={Dashboard} />
+      <Route path="/login/" component={SignIn} />
+
     </div>
   </Router>,
   document.getElementById('root')
--- frontend/src/sign-in/SignIn.js
+++ frontend/src/sign-in/SignIn.js
@@ -26,7 +26,7 @@ const styles = theme => ({
     },
   },
   paper: {
-    marginTop: theme.spacing.unit * 8,
+    marginTop: theme.spacing.unit * 16,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -43,47 +43,94 @@ const styles = theme => ({
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
+  split: {
+    marginTop: theme.spacing.unit * 3,
+  },
+
 });
 
-function SignIn(props) {
-  const { classes } = props;
+class SignIn extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  }
 
-  return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input name="password" type="password" id="password" autoComplete="current-password" />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  onSignIn() {}
+  onSignUp() {}
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
             Sign in
-          </Button>
-        </form>
-      </Paper>
-    </main>
-  );
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={this.handleChange('email')}
+                value={this.state.email}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.handleChange('password')}
+                value={this.state.password}
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={() => this.onSignIn()}
+            >
+              Sign in
+            </Button>
+            <Typography variant="subtitle1" className={classes.split} align="center">
+              or
+            </Typography>
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+              onClick={() => this.onSignUp()}
+            >
+              Sign Up
+            </Button>
+
+          </form>
+        </Paper>
+      </main>
+    );
+  }
 }
 
 SignIn.propTypes = {
