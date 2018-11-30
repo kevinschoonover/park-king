40cb3966a0c2bf1273f28bd69f42b70cf39783c5
Henry Wong <cywongx@gmail.com>
2018-11-30 17:23:05 -0500

    Add slash at end of route names to be accessible

--- backend/backend/factory.py
+++ backend/backend/factory.py
@@ -5,29 +5,29 @@ from flask import Flask
 def create_app():
     app = Flask('park-king-backend')
 
-    return app
-
-    @app.route('/user')
+    @app.route('/user/')
     def user():
       userObj = '{"id":0,"email":"example@mst.edu","password":"d2#2df@1ffkjb@"}'
       return userObj
 
-    @app.route('/vehicle')
+    @app.route('/vehicle/')
     def vehicle():
       vehicleObj = '{"id":0,"user_id":0,"type_id":0,"state":"TX","license":"ZX23YSB","make":"Tesla","year":"2017}'
       return vehicleObj
 
-    @app.route('/reservation')
+    @app.route('/reservation/')
     def reservation():
       reservationObj = '{"vehicle_id":0,"lot_id":0,"start_time":"6:00","end_time":"10:00"}'
       return reservationObj
 
-    @app.route('/lot')
+    @app.route('/lot/')
     def lot():
       lotObj = '{"id":0,"name":"A","location":"west"}'
       return lotObj
 
-    @app.route('/vehicleType')
+    @app.route('/vehicleType/')
     def vehicleType():
       vehicleType  = '{"id":0,"name":"sedan"}'
       return vehicleType
+
+    return app
