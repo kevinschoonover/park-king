097d96f29e850d6d463a6ff292a1c31bc80fda34
Adam Gausmann <agausmann@fastmail.com>
2018-12-01 15:04:47 -0600

    Update queries

--- backend/backend/factory.py
+++ backend/backend/factory.py
@@ -23,18 +23,33 @@ def create_app():
 
     @app.route('/user/<int:userid>',methods=['GET'])
     def userSpecific(userid):
-      db = database.get_db()
-      user = db.query("SELECT email,password from user WHERE user.id = '$userid'")
+      user = database.query(
+          "SELECT email,password from user WHERE user.id = ?",
+          [userid],
+          single=True,
+      )
       #email = user[0]
       #password = user[1]
       #userObj = '{"id":'  + userid  + ',"email":' + email  + ',"password":' + password  + '}'
-      userObj = Flask.json.jsonify(user)
+      userObj = json.jsonify(user)
       return userObj
+
+  def user_vehicles():
+      pass
+
+  def user_tickets():
+      pass
+
+  def user_reservations():
+      pass
 
     @app.route('/vehicle/<int:vehicleid>',methods=['GET'])
     def vehicle(vehicleid):
-      db = database.get_db()
-      vehicle = db.query("SELECT user_id,type_id,state,license,make,model,year from vehicle WHERE vehicle.id = '$vehicleid'")
+      vehicle = database.query(
+          "SELECT user_id,type_id,state,license,make,model,year from vehicle WHERE vehicle.id = ?",
+          [vehicleid],
+          single=True,
+      )
       #user_id = vehicle[0]
       #type_id = vehicle[1]
       #state = vehicle[2]
@@ -42,37 +57,56 @@ def create_app():
       #make = vehicle[4]
       #year = vehicle[5]
       #vehicleObj = '{"id":' + vehicleid  + ',"user_id":' + user_id  + ',"type_id":' + type_id  + ',"state":' + state  + ',"license":' + license  + ',"make":' + make  + ',"year":' + year  + '}'
-      vehicleObj = Flask.json.jsonify(vehicle)
+      vehicleObj = json.jsonify(vehicle)
       return vehicleObj
 
     @app.route('/reservation/<int:vehicleid>',methods=['GET'])
     def reservation(vehicleid):
-      db = database.get_db()
-      reservation = db.query("SELECT lot_id,start_time,end_time from reservation WHERE reservation.vehicleid = '$vehicle_id'").row_factory
+      reservation = database.query(
+          "SELECT lot_id,start_time,end_time from reservation WHERE reservation.vehicle_id = ?",
+          [vehicleid],
+      )
       #lot_id = reservation[0]
       #start_time = reservation[1]
       #end_time = reservation[2]
       #reservationObj = '{' + '"vehicle_id":' + vehicle_id  + ',"lot_id":' + lot_id  + ',"start_time":' + start_time  + ',"end_time":' + end_time  + '}'
-      reservationObj = Flask.json.jsonify(reservation)
+      reservationObj = json.jsonify(reservation)
       return reservationObj
+
+  def lots():
+      pass
 
     @app.route('/lot/<int:lotid>',methods=['GET'])
     def lot(lotid):
-      db = database.get_db()
-      lot = db.query("SELECT name,location from lot WHERE lotid.id = '$lotid'")
+      lot = database.query(
+          "SELECT name from lot WHERE id = ?",
+          [lotid],
+          single=True,
+      )
       #name = lot[0]
       #location = lot[1]
       #lotObj = '{"id":' + lotid + ',"name":' + name  + ',"location":' + location  + '}'
-      lotObj = Flask.json.jsonify(lot)
+      lotObj = json.jsonify(lot)
       return lotObj
 
-    @app.route('/vehicleType/<int:vehicleid>',methods=['GET'])
+  def lot_spaces():
+
+    @app.route('/vehicletype/<int:vehicleid>',methods=['GET'])
     def vehicleType(vehicleid):
-      db = database.get_db()
-      vehicle = db.query("SELECT name from vehicletype WHERE vehicle_type.id = '$vehicleid'")
+      vehicle = database.query(
+          "SELECT name from vehicle_type WHERE id = ?",
+          [vehicleid],
+          single=True,
+      )
       #name = vehicle[0]
       #vehicleType  = '{"id":' + vehicleid + ',"name":' + name  + '}'
-      vehicletype = Flask.json.jsonify(vehicle)
+      vehicletype = json.jsonify(vehicle)
       return vehicleType
 
     return app
+
+    def tickets():
+        pass
+
+    def ticket():
+        pass
