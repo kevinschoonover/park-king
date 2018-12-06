15f56b25a1f7b96589c075156d9dd9d57015ebc1
Kevin Schoonover <schoonoverkevinm@gmail.com>
2018-12-06 02:12:23 -0600

    Fix ticket rendering + add stepSize to chart

--- backend/create_ticket.py
+++ backend/create_ticket.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
-import time
+import json
 import requests
+from datetime import datetime
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
 
 vehicle_id = int(input('Vehicle ID: '))
 lot_id = int(input('Lot ID: '))
+print(datetime.now())
+data = {
+    'vehicle_id': vehicle_id,
+    'lot_id': lot_id,
+    'device_id': 1,
+    'time': datetime.utcnow(),
+    'auth_token': 'DEADBEEF',
+}
 
-requests.post(
-    'http://localhost:5000/tickets/',
-    json={
-        'vehicle_id': vehicle_id,
-        'lot_id': lot_id,
-        'device_id': 1,
-        'time': int(time.time()),
-        'auth_token': 'DEADBEEF',
-    },
+r = requests.post(
+    'http://localhost/tickets/',
+    data=json.dumps(data, cls=DateTimeEncoder),
+    headers={'Content-type': 'application/json'}
 )
