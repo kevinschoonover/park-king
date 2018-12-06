c17c93e680c341718a14e736c9b29eabe12aae15
Adam Gausmann <agausmann@fastmail.com>
2018-12-06 06:07:50 -0600

    Upgrade ticket creation

--- backend/create_ticket.py
+++ backend/create_ticket.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
 
-import json
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 
-
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, datetime):
-            return o.isoformat()
-
-        return json.JSONEncoder.default(self, o)
-
+vehicles = requests.get('http://localhost/vehicles/').json()
+vehicles.sort(key=lambda x: x['id'])
+for vehicle in vehicles:
+    print('{}: {} {} {} ({})'.format(
+        vehicle['id'],
+        vehicle['year'],
+        vehicle['make'],
+        vehicle['model'],
+        vehicle['license'],
+    ))
 
 vehicle_id = int(input('Vehicle ID: '))
+
+lots = requests.get('http://localhost/lots/').json()
+lots.sort(key=lambda x: x['id'])
+for lot in lots:
+    print('{}: {}'.format(lot['id'], lot['name']))
+
 lot_id = int(input('Lot ID: '))
+
 print(datetime.now())
+
 data = {
     'vehicle_id': vehicle_id,
     'lot_id': lot_id,
     'device_id': 1,
-    'time': datetime.utcnow(),
+    'time': datetime.now(tz=timezone.utc).isoformat(),
     'auth_token': 'DEADBEEF',
 }
 
-r = requests.post(
-    'http://localhost/tickets/',
-    data=json.dumps(data, cls=DateTimeEncoder),
-    headers={'Content-type': 'application/json'}
-)
+r = requests.post('http://localhost/tickets/', json=data)
